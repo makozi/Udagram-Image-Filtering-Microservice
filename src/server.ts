82286c449faf4ee96b33e5a1a88c3e541d52bf2e
Makozi <TAJ285@tajbank.com>
2022-09-08 11:35:05 +0100

    Resolved cannot GET Image

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,51 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { IndexRouter } from './controllers/v0/index.router';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // Root URI call
+  
+
+  /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    const imageUrl = req.query.image_url;
+
+    // check imageUrl is valid
+    if (!imageUrl) {
+      return res.status(400).send({
+        message: "The image url is required or malformed"
+      });
+    }
+
+    try {
+      console;
+      const filteredImageFromURL = await filterImageFromURL(imageUrl);
+      res.sendFile(filteredImageFromURL, () =>
+        deleteLocalFiles([filteredImageFromURL])
+      );
+    } catch (error) {
+      res.sendStatus(422).send("Unable to process image at the provided url");
+    }
+  });
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    res.status(200).json({
-      status: "OK",
-      message: "try GET /api/v0/"
-    });
+    res.send("try GET /filteredimage?image_url={{}}");
   });
 
-  app.use('/api/v0/', IndexRouter)
-
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
